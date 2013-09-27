e86fe0cd58d366e1f4f35182caab2c6384b3c218
Fernando Paredes <nano@fdp.io>
2013-09-27 14:26:28 -0700

    Add basic pdf renderer to send a pdf as a file

---
+++ test/integration/pdf_delivery_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PDFerTest < ActionDispatch::IntegrationTest
+  test "pdf request sends a pdf as a file" do
+    get home_path(format: :pdf)
+
+    assert_match "PDF", response.body
+    assert_equal "binary", headers["Content-Transfer-Encoding"]
+
+    assert_equal "attachment; filename=\"contents.pdf\"",
+      headers["Content-Disposition"]
+    assert_equal "application/pdf", headers["Content-Type"]
+  end
+end
